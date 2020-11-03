ad9312064e9d3779a062dd04df4b80f29e540f05
czyzeyong <cai_zeyong@hzlh.com>
2020-11-03 14:07:30 +0800

    feat:添加服务/路由的按tag分组/通过tag 筛选,添加路由的按照scope分组和按照apply id 分组,添加基本菜单的中英文i18n支持

---
+++ assets/js/app/core/directives/SortedTags.js
@@ -0,0 +1,80 @@
+
+(function() {
+  'use strict';
+
+  angular.module('frontend.core.directives')
+    .directive('sortedTags', function directive() {
+      return {
+        restrict: 'E',
+        scope: {
+          list: '=',
+          tagChange: '&onChange'
+        },
+        replace: true,
+        template : `<div class="sorted-tags">
+          <div ng-repeat="item in tags" class="sorted-tags__item">
+            <div><i>{{ item.name }}</i></div>
+            <div>
+              <span ng-repeat="tag in item.children" ng-click="toggleTag($event,tag)">{{ tag }}</span>
+            </div>
+          </div>
+        </div>`,
+        controller: [
+          '$scope',
+          function controller($scope) {
+            $scope.tags = getSortedTagsByGroup($scope.list || [])
+            $scope.$watch('list', function(newValue, oldValue){
+              $scope.tags = getSortedTagsByGroup(newValue || [])
+              $scope.selectedTags = []
+              $scope.tagChange({list: $scope.list})
+            })
+            $scope.selectedTags = []
+            $scope.toggleTag = function ($event, tag) {
+              var index = $scope.selectedTags.indexOf(tag)
+              if (index > -1) {
+                $scope.selectedTags.splice(index, 1)
+                $event.target.classList.remove('active')
+              } else {
+                $scope.selectedTags.push(tag)
+                $event.target.classList.add('active')
+              }
+              var result = $scope.list.filter(function(item) {
+                return $scope.selectedTags.every(function(tag){
+                  return item.tags.includes(tag)
+                })
+              })
+              $scope.tagChange({list: result})
+            }
+            $scope.tagChange({list: $scope.list})
+
+
+            function getSortedTagsByGroup (list) {
+              var allTags = Array.from(new Set(list.flatMap(function(item){
+                return item.tags || []
+              }))).sort();
+
+              var groupTags = {}
+              allTags.map(function(tag){
+                var c = tag.charAt(0)
+                if (!groupTags[c]) {
+                  groupTags[c] = {
+                    name: c,
+                    children: []
+                  }
+                }
+                groupTags[c].children.push(tag)
+              })
+
+              let result = []
+              Object.keys(groupTags).sort().map(function(k){
+                result.push(groupTags[k])
+              })
+
+              return result
+            }
+          }
+        ]
+      };
+    })
+  ;
+}());
